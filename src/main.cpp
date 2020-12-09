e5062064e13c5981470f925518736dcb2ba1d198
justin caseof <justin.caseof@gmail.com>
2020-12-10 00:07:53 +0100

    ir receive initially working on D3

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,87 @@
 #include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
 
-int LED_blue = LED_BUILTIN;
+int LED_blue = LED_BUILTIN; // == D4
 int LED_ON = 0;
 int LED_OFF = 1;
 int run = 0;
 char buf[64];
 
+int RECV_PIN = D3; //an IR detector connected to D3
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
 void setup() {
     Serial.begin(9600);
     pinMode(LED_blue, OUTPUT);
-  
+    irrecv.enableIRIn(); // Start the receiver
+}
+
+void dump(decode_results *results) {
+  // Dumps out the decode_results structure.
+  // Call this after IRrecv::decode()
+  uint16_t count = results->rawlen;
+  if (results->decode_type == UNKNOWN) {
+    Serial.print("Unknown encoding: ");
+  } else if (results->decode_type == NEC) {
+    Serial.print("Decoded NEC: ");
+  } else if (results->decode_type == SONY) {
+    Serial.print("Decoded SONY: ");
+  } else if (results->decode_type == RC5) {
+    Serial.print("Decoded RC5: ");
+  } else if (results->decode_type == RC5X) {
+    Serial.print("Decoded RC5X: ");
+  } else if (results->decode_type == RC6) {
+    Serial.print("Decoded RC6: ");
+  } else if (results->decode_type == RCMM) {
+    Serial.print("Decoded RCMM: ");
+  } else if (results->decode_type == PANASONIC) {
+    Serial.print("Decoded PANASONIC - Address: ");
+    Serial.print(results->address, HEX);
+    Serial.print(" Value: ");
+  } else if (results->decode_type == LG) {
+    Serial.print("Decoded LG: ");
+  } else if (results->decode_type == JVC) {
+    Serial.print("Decoded JVC: ");
+  } else if (results->decode_type == AIWA_RC_T501) {
+    Serial.print("Decoded AIWA RC T501: ");
+  } else if (results->decode_type == WHYNTER) {
+    Serial.print("Decoded Whynter: ");
+  } else if (results->decode_type == NIKAI) {
+    Serial.print("Decoded Nikai: ");
+  }
+  serialPrintUint64(results->value, 16);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): {");
+
+  for (uint16_t i = 1; i < count; i++) {
+    if (i % 100 == 0)
+      yield();  // Preemptive yield every 100th entry to feed the WDT.
+    if (i & 1) {
+      Serial.print(results->rawbuf[i] * kRawTick, DEC);
+    } else {
+      Serial.print(", ");
+      Serial.print((uint32_t) results->rawbuf[i] * kRawTick, DEC);
+    }
+  }
+  Serial.println("};");
 }
 
 void doStuff() {
+    if (irrecv.decode(&results)) {
+        Serial.println(">>>>>>");
+
+        dump(&results);
+
+        irrecv.resume(); // Receive the next value
+    }
+
 }
 
 void loop() {
@@ -23,9 +92,9 @@ void loop() {
         doStuff();
 
         digitalWrite(LED_blue, LED_ON);
-        delay(1000);
+        delay(100);
         digitalWrite(LED_blue, LED_OFF);
-        delay(1000);
+        delay(100);
     }
 }
 
